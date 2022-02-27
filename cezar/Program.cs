f61d73f22a3c87e52339e73a6e163b03c50f28fa
DarkTuck <98091387+DarkTuck@users.noreply.github.com>
2022-02-27 15:30:09 +0100

    Dodaj pliki projektów.

---
+++ cezar/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace cezar
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("not Copyrighted by DT");
+            //zminne z yekstem jawnym i nie jawnym
+            string tekstJawny = "";
+            string tekstNieJawny = "";
+            //zminne z alfabetem i krokiem
+            string alfabet = "aąbcćdeęfghijklłmnńoóprsśtuwyzżź";
+            //tekst do szyfrowania
+            int krok = 3;
+            Console.WriteLine("Podaj tekst do zaszyfrowania:");
+            tekstJawny = Console.ReadLine();
+            for(int licznik=0; licznik<tekstJawny.Length; licznik++)
+            {
+                char znak = tekstJawny[licznik];
+                int numerZnaku = alfabet.IndexOf(znak);
+                if (numerZnaku == -1)
+                {
+                    tekstNieJawny += znak;
+                } else
+                {
+                    int nowyNumerZnaku = (numerZnaku + krok) % alfabet.Length;
+                    char nowyZnak = alfabet[nowyNumerZnaku];
+                    tekstNieJawny += nowyZnak;
+                 }
+                
+            }
+            Console.WriteLine(tekstNieJawny);
+        }
+    }
+}
